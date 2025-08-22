f03c45283b0043c3d8917a23b22ad8a1542363b7
Kunal Madoliya <madoliyakunal2005@gmail.com>
2025-08-22 18:37:22 +0530

    finalChanges

--- src/components/ui/Carousel.jsx
+++ src/components/ui/Carousel.jsx
@@ -25,7 +25,7 @@ function Carousel({ products = [], perView = 3, title = "" }) {
   const settings = {
     infinite: true,
     centerMode: true,
-    centerPadding: "20px",
+    centerPadding: "10px",
     slidesToShow: perView,
     swipeToSlide: true,
     nextArrow: <NextArrow />,
@@ -42,14 +42,21 @@ function Carousel({ products = [], perView = 3, title = "" }) {
         breakpoint: 1024, // lg
         settings: {
           slidesToShow: Math.min(perView, 2),
-          centerPadding: "25px",
+          centerPadding: "20px",
         },
       },
       {
-        breakpoint: 768, // md
+        breakpoint: 768, // md (tablet)
+        settings: {
+          slidesToShow: 2,
+          centerPadding: "10px",
+        },
+      },
+      {
+        breakpoint: 640, // sm (mobile)
         settings: {
           slidesToShow: 1,
-          centerPadding: "20px",
+          centerPadding: "0px",
         },
       },
     ],
@@ -60,21 +67,21 @@ function Carousel({ products = [], perView = 3, title = "" }) {
       {title && <h2 className="text-center text-xl font-bold mb-6">{title}</h2>}
       <Slider {...settings}>
         {products.map((product, index) => (
-          <div key={index} className="p-4">
-            <div className="bg-white w-full max-w-[280px] sm:max-w-[300px] md:max-w-[340px] rounded-xl border shadow-lg overflow-hidden mx-auto">
+          <div key={index} className="px-2 sm:px-4">
+            <div className="bg-white p-5 w-full max-w-[280px] sm:max-w-[320px] md:max-w-[340px] rounded-xl border shadow-lg overflow-hidden mx-auto">
               <img
                 src={product.image}
                 alt={product.name}
                 className="w-full h-48 sm:h-56 md:h-64 object-contain object-center"
               />
               <div className="p-4 text-center">
-                <h3 className="text-sm md:text-lg font-semibold">
+                <h3 className="text-sm sm:text-base md:text-lg font-semibold">
                   {product.name}
                 </h3>
                 <div className="flex items-center justify-center gap-2">
                   <p className="text-gray-600">₹{product.price}</p>
                   {product.oldPrice && (
-                    <p className="text-gray-600 line-through">
+                    <p className="text-gray-500 line-through">
                       ₹{product.oldPrice}
                     </p>
                   )}
